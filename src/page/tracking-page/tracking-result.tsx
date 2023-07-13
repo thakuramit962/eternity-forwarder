ee8028fc47d157f1f682deaa1a6a386cd8bf6aed
Amit Thakur <thakuramit962@gmail.com>
2023-07-13 17:48:05 +0530

    demo api

--- src/page/tracking-page/tracking-result.tsx
+++ src/page/tracking-page/tracking-result.tsx
@@ -23,7 +23,7 @@ export default function TrackingResult() {
 
     useEffect(() => {
         setFetching(true)
-        fetch(`http://app.shipbuddy.co.in/api/get-lrtimeline/${trackId}`)
+        fetch(`https://app.shipbuddy.co.in/api/get-lrtimeline/${trackId}`)
             .then(response => {
                 if (!response.ok) throw new Error('Network response was not ok')
                 return response.json()
